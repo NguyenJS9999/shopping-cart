ac7278ff3b5105bde7e883ce90be3db26d1f3b2b
NguyenKeo <nguyentrungnguyenth14@gmail.com>
2021-09-09 22:31:05 +0700

    React Js buoi 10 kiem tra giua ky- chưa deep copy

--- src/shopping-cart/payment.js
+++ src/shopping-cart/payment.js
@@ -1,7 +1,40 @@
 import './payment.css'
+import React, {  useEffect, useState } from "react";
+// import { DATA_ITEMS } from './mock-data'
 
-export function PaymentElement( {totalAmount} ) {
-    let grossMoney = totalAmount - totalAmount * 0.1;
+// {totalAmount}
+export function PaymentElement( {stateCssButtonContinueBuy} ) {
+    // const [ stateDataItems, setDataItems] = useState( [...DATA_ITEMS] );
+    const [ stateTotalAmount, setTotalAmount] = useState( 0 );
+    let grossMoney = stateTotalAmount + (stateTotalAmount * 0.1);
+
+    // Data deep copy sau thay đổi số lượng item
+
+    useEffect( () => {
+        let totalAmounts = [ ];
+        let totalUnitPrice;
+        // [...DATA_ITEMS].map( getMoney )
+        [...stateCssButtonContinueBuy].map( getMoney )
+
+        function getMoney (item) {   
+            // console.log('Đơn giá item', item.price_product )
+            // console.log('Số lượng sp đó', item.quantity )
+            
+            totalUnitPrice = (item.price_product) * (item.quantity)
+            // console.log( '1 tổng đơn giá', totalUnitPrice )
+            totalAmounts.push( totalUnitPrice );
+        } 
+        
+        // console.log('Mảng các đơn giá', totalAmounts)
+        const reducer = (previousValue, currentValue) => previousValue + currentValue;
+        let sumMoney = totalAmounts.reduce( reducer ) ;
+        // let sumMoney = 0;
+        // for ( let i = 0; i <  totalAmounts.length; i++ ) {
+        //     sumMoney += totalAmounts[i]
+        // } 
+        // console.log('sumMoney', sumMoney);
+        setTotalAmount( sumMoney )
+    }, [setTotalAmount, stateCssButtonContinueBuy]);
 
     return (
         <section className=" payment-container  container ">
@@ -9,7 +42,7 @@ export function PaymentElement( {totalAmount} ) {
             <div className=" count-money ">
             <span className=" total-money ">
                 <b>Tổng tiền</b>
-                <div className=" total-money-number "> {totalAmount.toLocaleString()} VNĐ </div>
+                <div className=" total-money-number "> {stateTotalAmount.toLocaleString()} VNĐ </div>
             </span>
             {/*  */}
             <div className="border-bottom" />
@@ -20,7 +53,7 @@ export function PaymentElement( {totalAmount} ) {
             {/*  */}
             <div className="border-bottom" />
             <span className="into-money">
-                <b>Thành tiền</b>
+                <b>Thành tiền sau thuế</b>
                 <div className="into-money-number"> {grossMoney.toLocaleString()} VNĐ</div>
             </span>
             </div> {/* count-money */}
--- src/shopping-cart/shopping-cart.js
+++ src/shopping-cart/shopping-cart.js
@@ -1,71 +1,55 @@
-
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './shopping-cart.css';
 import { PaymentElement } from './payment';
-
-const DATA_ITEMS = [
-    {
-        id: 1,
-        image_product: 'https://product.hstatic.net/1000367569/product/sub_pcs_318nb_c6dba44f27aa4fbcaa77210701c481db_master.jpg',
-        name_product: 'PCS 318NB',
-        price_product: 65200000,
-        quantity: 1,
-    },
-    {
-        id: 2,
-        image_product: 'https://www.nexo-sa.com/wp-content/uploads/ls18-e1476457570490.jpg',
-        name_product: 'LS18',
-        price_product: 89612600,
-        quantity: 2,
-    },
-    {
-        id: 3,
-        image_product: 'https://product.hstatic.net/1000367569/product/sub_pcs_218_bk_200d595b6d5c4faea5437d34a2ef63fa_master.jpg',
-        name_product: 'PCS 218BK',
-        price_product: 33000000,
-        quantity: 3,
-    },
-  
-]
-
-
+import { DATA_ITEMS } from './mock-data'
 
 export function Cart() {
 
     const [ stateDataItems, setDataItems] = useState( [...DATA_ITEMS] );
-    // Tổng tiền các sản phẩm muốn mua - Hiên chưa cộng dồn được
-    const [ stateTotalAmount, setTotalAmount] = useState( 195600000 );
+    const [ stateCssButtonDelete, setCssButtonDelete] = useState( false );
+    const [ stateCssButtonContinueBuy, setCssButtonContinueBuy] = useState( true );
+    const [ stateUpdateTotalMoney, setUpdateTotalMoney] = useState( null );
+
+    
+    const dataLength = DATA_ITEMS.length;
 
     // Tăng
     function addProductNumber( event, id ) { console.log('Tăng')
-        // clone state, để dễ dàng chỉnh sửa giá trị
-        let cloneDataItems =[...stateDataItems]
-        cloneDataItems.forEach( (item, index)=> {
+        let cloneDataItems = stateDataItems.map( (obj) => ( {...obj}) );
+
+        cloneDataItems.forEach( (item)=> {
             if(item.id === id){
                 item.quantity += 1
             }
         });
-        // set lại giá trị state với giá trị mới là clone,
+        console.log('cloneDataItems', cloneDataItems)
         setDataItems(cloneDataItems)
+        // Truyền obj data mới vào state-truyền snag payment
+        setUpdateTotalMoney(cloneDataItems)
     }
     // Giảm
     function reduce_product_number( event, id ) { console.log('Giảm')
-        let cloneDataItems = [...stateDataItems]
-        cloneDataItems.forEach( ( item, index) => {
+        let cloneDataItems = stateDataItems.map( (obj) => ( {...obj}) );
+        cloneDataItems.forEach( ( item) => {
             if ( item.id === id ) {
                 item.quantity -= 1
             }
         }) 
-        setDataItems(cloneDataItems)
+        console.log('cloneDataItems', cloneDataItems)
+        setDataItems(cloneDataItems);
+        // Truyền obj data mới vào state-truyền snag payment
+        setUpdateTotalMoney(cloneDataItems);
+
+        
     }
-    // Lấy giá trị ô input
+    // Lấy giá trị ô input số lượng item
     function valueInputQuantity(event, id) {
         let valueInputQuantity = event.target.value;
-        let cloneDataItems 
+        let cloneDataItems; 
         
         if ( valueInputQuantity > 0 &&  valueInputQuantity <= 100 )  {
-            cloneDataItems = [...stateDataItems];
+            cloneDataItems = stateDataItems.map( (obj) => ( {...obj}) );
             cloneDataItems.forEach( ( item ) => {
                 if ( item.id === id ) {
                     item.quantity = parseInt( valueInputQuantity );
@@ -75,46 +59,53 @@ export function Cart() {
             return;
         }     
         setDataItems(cloneDataItems)
+        // Truyền obj data mới vào state-truyền snag payment
+        setUpdateTotalMoney(cloneDataItems)
     }
+
     // Xóa
-    function delete_item(id) {
+    function deleteItem(id) {
         console.log('Xóa 1 sản phẩm đó id: ', id);
-        let deletedItem = [...DATA_ITEMS].filter((item) => item.id !== id);
+        let deletedItem = stateDataItems.filter( (item) => item.id !== id);
         console.log('Sau xóa còn deletedItem', deletedItem)   
         setDataItems( deletedItem );
-      }
+       
+    }
+
+    // let deletedItem2 = stateDataItems.filter( function delete() );
+    // function delete( (item) => item.id !== id )  
 
     // Xóa tất cả các item
     function deleteAllItems() { 
         // let deleteAllItem = [...DATA_ITEMS].filter( (item) => item.id === id );   
         // setDataItems(deleteAllItem)
-        let deleteItem =  [...DATA_ITEMS].splice( 0, DATA_ITEMS.length  );
+        let deleteItem =  [...stateDataItems].splice( 1, 0 );
         console.log('Xóa tất cả các Item deleteItem', deleteItem)
-        setDataItems([...DATA_ITEMS].splice( 0, DATA_ITEMS.length  ))
+        setDataItems( deleteItem )
+        console.log('stateDataItems sau xóa tất cả', stateDataItems);
+        
+        if ( dataLength <= 0) {
+            setCssButtonDelete(false)
+            setCssButtonContinueBuy(true)
+        } else {
+            setCssButtonDelete(true)
+            setCssButtonContinueBuy(false)
+        }
     }
     // Tiếp tục mua sắm
     function continueShopping() {
         console.log('Tiếp tục mua hàng, reset lại sản phẩm về ban đầu');
-        window.location.reload();
+        // window.location.reload();
         let dataItem = [...DATA_ITEMS];
         setDataItems(dataItem)
+        setCssButtonDelete(false);
+        setCssButtonContinueBuy(true)
     }
 
-    // let totalValue = DATA_ITEMS.map( ( cart_item)  => {
-    //     console.log('cart_item.price_product', cart_item.price_product);
-    //     let total
-    // });
-    // console.log('totalValue', totalValue)
-    // setTotalAmount(totalValue)
-    // cart_item.price_product * cart_item.quantity
-
-    const cartItemElement = DATA_ITEMS.map( ( cart_item, id ) => 
+    const cartItemElement = stateDataItems.map( (cart_item ) => 
 
         <div  key = { cart_item.id }
             className=   {` cart-item   `} >
-            {/* Chọn sp */}
-            <span> <input type="checkbox" defaultValue /> </span>
-        
             {/* Ảnh sp */}
             <span className=" cart-item-product-image ">
                 <img src={ cart_item.image_product } alt="sound" />
@@ -122,27 +113,27 @@ export function Cart() {
         
             {/* Tên sp */}
             <span> { cart_item.name_product } </span>
-
-            {/* Đơn giá sp    */}
-            <span> { ( cart_item.price_product.toLocaleString() ) } VNĐ </span>
-
             {/* Giảm tăng số lượng sp */}
             <span className="custom-number">
                 <i onClick= { (event) => reduce_product_number( event, cart_item.id ) } className="fas fa-minus" />
                 
-                {/* quantity_product  stateNumberProducts */}
-                <input onChange = { (event) => valueInputQuantity(event, cart_item.id ) }
+                {/* Ô nhập số lượng sản phẩm */}
+                <input 
+                    onChange = { (event) => valueInputQuantity(event, cart_item.id ) }
                     value = { cart_item.quantity }
-                    className="custom-number-input" type="number"  /> 
+                    
+                    className="custom-number-input" type="number"  
+                /> 
                 
                 <i onClick= { (event) => addProductNumber( event, cart_item.id ) } className="fas fa-plus" /> 
             </span>
-
+            {/* Đơn giá sp    */}
+            <span> { ( cart_item.price_product.toLocaleString() ) } VNĐ </span>
             {/* Thànhs tiền */}
             <span> { (cart_item.price_product * cart_item.quantity ).toLocaleString() } VNĐ</span>
 
             {/* Nút xóa */}
-            <span onClick = { () => delete_item( cart_item.id ) }
+            <span onClick = { () => deleteItem( cart_item.id ) }
                 className= {` delete-one  `} type="button" > 
                 <i className="fas fa-trash-alt " /> 
             </span>
@@ -160,43 +151,41 @@ export function Cart() {
                 </div>
 
                 <div className="cart-title">
-                <span> 
-                    <input type="checkbox" defaultValue /> 
-                </span>
                 
                 <span> SẢN PHẨM&nbsp;
                     <small className="cart-title-number-items">
                        ({DATA_ITEMS.length}) 
                     </small> 
                 </span>
-                
-                <span> TÊN SẢN PHẨM</span>
-                
-                <span>ĐƠN GIÁ</span>
-                
+                <span>TÊN SẢN PHẨM</span>
                 <span>SỐ LƯỢNG</span>
-                
+                <span>ĐƠN GIÁ</span>
                 <span>THÀNH TIỀN</span>
                 {/* Recbin */}
                 <span style={{visibility: 'hidden'}}> <i className="fas fa-trash-alt" /> </span>
                 </div>
                 {/* Cart Item */}
-                { stateDataItems.length > 0  ? (cartItemElement) : (<p className="cart-feedback">Không tìm thấy sản phẩm phù hợp!</p>)  }
+                { stateDataItems.length > 0  ? (cartItemElement) : (<p className="cart-feedback">Giỏ hàng không còn sản phẩm nào!</p>)  }
 
                 
                 <div className=" cart-button-buy-delete-all ">
+
                     <div onClick= { continueShopping }> 
-                        <span className="button-buy" type="button"> TIẾP TỤC MUA HÀNG </span> 
+                        <span className= {`${stateCssButtonContinueBuy ? 'd-none' : 'd-flex' } button-buy`} type="button"> TIẾP TỤC MUA HÀNG </span> 
                     </div>
 
                     <span onClick= { deleteAllItems }
-                        className="button-delete-all" type="button" > XÓA TOÀN BỘ </span>
+                        className= {` ${ stateCssButtonDelete ? 'd-none' : 'd-flex'} button-delete-all `}  
+                        type="button" > 
+                        XÓA TOÀN BỘ 
+                    </span>
                 
                 </div>
             
             </section>
 
-            <PaymentElement totalAmount = {stateTotalAmount}/>
+            {/* totalAmount = {stateTotalAmount} */}
+            <PaymentElement stateCssButtonContinueBuy = {stateUpdateTotalMoney} />
             </>
     )
 }
